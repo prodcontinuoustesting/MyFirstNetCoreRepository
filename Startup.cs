9baf14ed22e33b1050c90e72dd2a901b6260f476
prodcontinuoustesting <65500931+prodcontinuoustesting@users.noreply.github.com>
2020-05-17 14:35:49 -0500

    This is a testing commit

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+// Testing Continuous Integration (CI)
+
 namespace pipelines_dotnet_core
 {
     public class Startup
